6fe00ff99566bf9da682fe55265b410c843aac8b
Suryaa34 <suryaadisyaputra34@gmail.com>
2025-08-17 16:27:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function playMusic() {
+  const music = document.getElementById("bg-music");
+  music.play();
+}
+
+const targetDate = new Date("Nov 30, 2025 09:00:00").getTime();
+
+const timer = setInterval(function() {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + " Hari " + hours + " Jam " + minutes + " Menit " + seconds + " Detik ";
+
+  if (distance < 0) {
+    clearInterval(timer);
+    document.getElementById("countdown").innerHTML = "Acara telah dimulai!";
+  }
+}, 1000);
